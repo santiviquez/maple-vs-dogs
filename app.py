4289d0fc12a4739aedf702668953bd2f2d37da68
Santiago Viquez <santi.viquez@gmail.com>
2020-07-30 18:57:03 -0600

    update

--- app.py
+++ app.py
@@ -2,32 +2,29 @@ import streamlit as st
 from fastai import *
 from fastai.vision import *
 from PIL import Image
-
 st.set_option('deprecation.showfileUploaderEncoding', False)
+export_file_url_local = Path('./')
 
-st.title('Maple vs Dogs')
+def predict(image_file):
+    img = open_image(image_file)
+    learn.predict(img)
+    prediction = learn.predict(img)[0].obj
+    return prediction
 
-uploaded_file = st.file_uploader("text here", type=['png', 'jpg'])
+
+st.title('Maple not Maple')
+
+uploaded_file = st.file_uploader("Select an image (preferably a dog one)", type=['png', 'jpg'])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
-    st.write("Classifying...")
 
-
-
-
-
-
-
-    #export_file_url = Path('https://drive.google.com/file/d/1PnK5-fhDRr_9xBc1PqErQTdyh_jdbXRv/view?usp=sharing')
-    export_file_url_local = Path('./')
-
+    # Load trained model 
     learn = load_learner(export_file_url_local)
+    # Get predicted class
+    prediction = predict(uploaded_file)
 
-    img = open_image(uploaded_file)
-
-    learn.predict(img)
-
-    pred_class = learn.predict(img)[0].obj
-    st.text(pred_class)
+    if prediction != 'maple':
+        st.title('Not Maple :(')
+    else:
+        st.title('Maple!')
